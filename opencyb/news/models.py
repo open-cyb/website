e239a390a1642933afd80d87b73bd88131ca77c6
bpqvgq <bpqvgq@gmail.com>
2021-07-03 18:05:33 +0300

    Add markdown for news content

--- opencyb/news/models.py
+++ opencyb/news/models.py
@@ -9,7 +9,7 @@ STATUS = (
 class New(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
+    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='news_posts')
     updated_on = models.DateTimeField(auto_now= True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
