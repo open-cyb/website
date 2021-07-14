815c77e7d079f780a86c32d8fa7094387c2852b1
bpqvg <bpqvgq@gmail.com>
2021-07-14 18:50:47 +0300

    Some fixed

--- opencyb/articles/templatetags/customtags.py
+++ opencyb/articles/templatetags/customtags.py
@@ -22,12 +22,12 @@ def preview(value):
 
 @register.filter
 def html_edit(value):
-    soup = BeautifulSoup(value)
+    soup = BeautifulSoup(value, "html.parser")
+    
     # Add id to image (for open by click)
     for img in soup.find_all('img'):
         img['class'] = 'popup-image'
     
-    # <iframe width="560" height="315" src="https://www.youtube.com/embed/JihGr_ozNG4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     # Replace all youtube-link to iframe video integrated to html code
     for a in soup.find_all(text="youtube-link"):
         link = a.parent['href']
@@ -39,16 +39,9 @@ def html_edit(value):
         a.parent['frameborder'] = '0'
         a.parent['allow'] = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
         a.parent['allowfullscreen'] = None
-        # a.parent['class'] = 'youtube-video'
-        # a.parent.name='iframe'
-        # a.parent['max-width']='100%'
-        # a.parent['height']='auto'
-        # a.parent['src']=link
-        # a.parent['title']='Youtube video player'
-        # a.parent['frameborder']='0'
-        # a.parent['allow']="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        # a.parent['allowfullscreen']=None
-        # a.parent['class'] = 'youtube-video'
+        
+    for link in soup.find_all('a', href=True):
+        link['target'] = '_blank'
     
     html_content = str(soup)
     
