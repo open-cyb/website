6a7d25ec847e91d770220ed109bd79ebb31e28ca
root <root@vm194940.pq.hosting>
2021-07-19 18:55:24 +0200

    Upload and edit news from site(not from admin panel)

--- opencyb/news/forms.py
+++ opencyb/news/forms.py
@@ -1,5 +1,29 @@
-from .models import Comment
+from .models import New, Comment
 from django import forms
+from django.contrib.auth.models import User
+from django.core.validators import validate_slug
+
+class NewForm(forms.ModelForm):
+    title = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Название..', 'style':'margin-bottom:15px;'}
+    ))
+    slug = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Slug..', 'style':'margin-bottom:15px;'}
+    ), validators=[validate_slug], required=False)
+    author = forms.ModelChoiceField(queryset=User.objects.all(), widget=forms.Select(
+        attrs={'class': 'form-control', 'style':'margin-bottom:15px;'}
+    ))
+    content = forms.CharField(widget=forms.Textarea(
+        attrs={'class': 'form-control', 'rows': '10', 'placeholder': 'Содержимое статьи..', 'style':'margin-bottom:15px;'}
+    ))
+    status = forms.ChoiceField(widget=forms.Select(
+        attrs={'class': 'form-control', 'style':'margin-bottom:15px;'}
+    ), choices=[ (0, "Draft"), (1, "Publish") ])
+    
+    class Meta:
+        model = New
+
+        fields = ('title', 'slug', 'author', 'content', 'status')
 
 class CommentForm(forms.ModelForm):
     # name = forms.CharField(label="Имя", required=True)
--- opencyb/news/views.py
+++ opencyb/news/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from .models import New
-from .forms import CommentForm
+from .forms import NewForm, CommentForm
 import django.views.generic as generic
 from django.http import HttpResponseRedirect
 from django.contrib import messages
+from django.utils.text import slugify
+from unidecode import unidecode
 
 class NewsList(generic.ListView):
     queryset = New.objects.filter(status=1).order_by('-created_on')
@@ -35,3 +37,53 @@ def new_detail(request, slug):
                                             'comments': comments,
                                             'new_comment': new_comment,
                                             'comment_form': comment_form })
+
+def new_edit(request, slug):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(request.build_absolute_uri('/'))
+    
+    template_name = 'news/new_edit.html'
+    context = {}
+    edit_form = NewForm(instance = New.objects.get(slug=slug))
+    context['form'] = edit_form
+
+    if request.method == 'GET':
+        return render(request, template_name, context)
+    
+    elif request.method == 'POST':
+        instance = get_object_or_404(New, slug=slug)
+        edit_form = NewForm(request.POST, instance=instance)
+
+        if edit_form.is_valid():
+            new_object = edit_form.save(commit=False)
+            new_object.slug = slugify(unidecode(new_object.title))
+            new_object.save()
+            return HttpResponseRedirect(request.build_absolute_uri('/news/' + new_object.slug))
+        
+        context['errors'] = edit_form.errors
+
+        return render(request, template_name, context)
+
+def new_upload(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(request.build_absolute_uri('/'))
+    
+    template_name = 'news/new_upload.html'
+    context = {}
+    context['form'] = NewForm()
+
+    if request.method == 'GET':
+        return render(request, template_name, context)
+    
+    elif request.method == 'POST':
+        a_form = NewForm(request.POST)
+
+        if a_form.is_valid():
+            new_object = a_form.save(commit=False)
+            new_object.slug = slugify(unidecode(new_object.title))
+            new_object.save()
+            return HttpResponseRedirect(request.build_absolute_uri('/news/' + new_object.slug))
+        
+        context['errors'] = a_form.errors
+
+        return render(request, template_name, context)
